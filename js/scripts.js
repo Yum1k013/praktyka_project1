fca567d268be20966fa1b2d0db1b9d029381775e
Your Name <you@example.com>
2024-05-13 12:31:50 +0200

    add navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 'use strict';
 
-const hamburger = document.querySelector('.material-icons');
-console.log(hamburger);
+const hamburger = document.querySelector('nav .material-icons');
+
+const close = document.querySelector('.navigation .material-icons')
+
+const menuDiv = document.querySelector('.navigation');
 
 hamburger.addEventListener('click', () => {
-    alert('test2');
+    menuDiv.classList.add('showMenu')
 });
+
+close.addEventListener('click', () =>{
+    menuDiv.classList.remove('showMenu');
+})
